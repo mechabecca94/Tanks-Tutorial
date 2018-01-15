c9089cffdb0236644d18f6a73beb17ce3bf44dcd
Rebecca <rebecgood@gmail.com>
2018-01-15 17:06:27 -0500

    YOUKILLEDME
    Experiment with split screen and surprising messages. Pushing the shoot button triggers a split screen and unsettling message. Really enjoy the juxtaposition, want to try fixing the code so that it checks after round 1 which tank was killed... maybe won't respawn the second tank (making it unplayable?)

---
+++ Assets/_Completed-Assets/Scripts/SplitScreenTest/SplitScreen.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SplitScreen : MonoBehaviour {
+	
+	public Camera Cam1;
+	
+	// Update is called once per frame
+	void Update () 
+	{
+
+		if (Input.GetKey(KeyCode.Space)) 
+		{
+		   Cam1.rect = new Rect(0.5f,0,0.5f,1f); 
+		}
+
+		  else 
+		  { 
+		  	Cam1.rect = new Rect(0,0,0,0); 
+		  }
+	}
+
+	/*public void ChangeSplitScreen()
+	{
+		Horizontal = !Horizontal;
+
+		if(Horizontal) {
+			Cam1.rect = new Rect(0,0,0,0);
+		}
+		else {
+			Cam1.rect = new Rect(0.5f,0,0.5f,1f);
+		}
+	}*/
+
+}
---
+++ Assets/_Completed-Assets/Scripts/SplitScreenTest/SplitScreen2.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SplitScreen2 : MonoBehaviour {
+	
+	public Camera Cam2;
+	
+	// Update is called once per frame
+	void Update () 
+	{
+
+		if (Input.GetKey(KeyCode.Return)) 
+		{
+		   Cam2.rect = new Rect(0,0,0.5f,1f); 
+		}
+
+		  else 
+		  { 
+		  	Cam2.rect = new Rect(0,0,0,0); 
+		  }
+	}
+
+	/*public void ChangeSplitScreen()
+	{
+		Horizontal = !Horizontal;
+
+		if(Horizontal) {
+			Cam1.rect = new Rect(0,0,0,0);
+		}
+		else {
+			Cam1.rect = new Rect(0.5f,0,0.5f,1f);
+		}
+	}*/
+
+}
