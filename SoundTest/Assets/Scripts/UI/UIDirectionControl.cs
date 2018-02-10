e3f313cb2e672d0350aa1ecf42ea60213b0bdf24
Rebecca <rebecgood@gmail.com>
2018-02-09 20:44:53 -0500

    Test of SoundTest
    reset

---
+++ SoundTest/Assets/Scripts/UI/UIDirectionControl.cs
@@ -0,0 +1,24 @@
+using UnityEngine;
+
+public class UIDirectionControl : MonoBehaviour
+{
+    public bool m_UseRelativeRotation = true;  
+
+
+    private Quaternion m_RelativeRotation;     
+
+
+    private void Start()
+    {
+        m_RelativeRotation = transform.parent.localRotation;
+    }
+
+
+    private void Update()
+    {
+        if (m_UseRelativeRotation)
+            transform.rotation = m_RelativeRotation;
+    }
+}
+
+//finds the rotation of the canvas, and then using that every updates set the current rotation to that rotation
---
+++ SoundTest/README.md
@@ -0,0 +1,7 @@
+# Tanks Tutorial Log
+
+## January 10 2018 ##
+Uploaded my in-progress tutorial file to GitHub, which is 1/2 done or 1/2 not done depending on your life outlook. 
+
+## January 08 2018 ##
+In the beginning their was GitHub.
