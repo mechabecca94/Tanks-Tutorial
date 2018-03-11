ed298d7efb74a502e0e5793c200e2c1eef6d4ad5
Rebecca <rebecgood@gmail.com>
2018-03-10 20:49:36 -0500

    THE HAND
    There is now a "human" hand in this game with "humans." They are very realistic "humans," totally don't look demonic at all...

--- Assets/LookAtObject.cs
+++ Assets/LookAtObject.cs
@@ -5,20 +5,23 @@ using UnityEngine;
 public class LookAtObject : MonoBehaviour {
 
 	GameObject target;
+	Vector3 targetPosition1;
 
 	// Use this for initialization
 	void Start () {
 		//target = GameObject.FindWithTag("Player1");
-		target = GameObject.Find("Survivor2");
+		target = GameObject.Find("LOOK");
 
 	}
 	
 	// Update is called once per frame
 	void Update () {
-		Vector3 targetPosition = new Vector3(target.transform.position.x,
+				target = GameObject.Find("LOOK");
+		Vector3 targetPosition1 = new Vector3(target.transform.position.x,
 											target.transform.position.y,
 											target.transform.position.z);
-		transform.LookAt(targetPosition);
+		transform.LookAt(targetPosition1);
+												
 	}
 }
 
---
+++ Assets/childLook.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class childLook : MonoBehaviour {
+ Camera cam1;
+ Camera cam2;
+
+	// Use this for initialization
+	void Start () {
+	  cam1 = GameObject.Find("Cam1").GetComponent<Camera>();
+	  cam2 = GameObject.Find("Cam2").GetComponent<Camera>();
+	 cam1.enabled = true;
+     cam2.enabled = false;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (Input.GetKeyDown(KeyCode.W)) {
+         cam1.enabled = true;
+         cam2.enabled = false;
+     }
+     if (Input.GetKeyDown(KeyCode.UpArrow)) {
+         cam2.enabled = true;
+         cam1.enabled = false;
+     }
+
+
+
+	}
+}
