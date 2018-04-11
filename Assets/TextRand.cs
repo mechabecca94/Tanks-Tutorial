8104000bf37cde2d1f2b053d3af1a04814ad3dfa
Rebecca <rebecgood@gmail.com>
2018-04-10 22:27:25 -0400

    Push!
    Pushing my changes - made the third panel, it's done :)

--- Assets/TextRand.cs
+++ Assets/TextRand.cs
@@ -6,18 +6,19 @@ using UnityEngine.UI;
 
 public class TextRand : MonoBehaviour {
 public Text display;
+public Camera Camera1; 
 string[] options = 
-{"*Cough cough*",
-"...",
+{"\n*Cough cough*",
+"\n...",
 "...You are a chosen child of Prefab...",
-"...You are not of this screen...",
+" ...You are not of this screen...",
 "...This Scene is your right...",
-"*wheeze*",
+"\n*wheeze*",
 "...Variables are counting on you...",
-"...Player one started it...",};
+" ...Player one started it...",};
 
-
-public int pressed = 0;
+//
+//public int pressed = 0;
 
 	// Use this for initialization
 	void Start () {
@@ -28,7 +29,12 @@ public int pressed = 0;
 	
 	// Update is called once per frame
 	void Update () {
-		
+		//    Ray ray = Camera1.ScreenPointToRay(Input.mousePosition/Screen.width);
+		//	if (Input.GetMouseButtonDown(0) /*&& Physics.Raycast(ray)==true*/){
+			//	NewRandom();
+			// Debug.Log(Input.mousePosition.ToString("F4"));
+		//	}
+
     /* if (Input.GetKey(KeyCode.Return)) 
 			{
 				if(GameObject.Find("byebye") == null) {
@@ -42,23 +48,31 @@ public int pressed = 0;
 		//}
 	}		
 	
-	 void OnMouseDown()
+
+	// void OnMouseDown()
+  //  {
+    //	if (Input.GetMouseButtonDown(0)) {
+	//		pressed = 0; }
+
+
+			 //if (Input.GetMouseButtonDown(0) && pressed == 0){
+				//NewRandom();
+		         //}
+
+//		     if (Input.GetMouseButtonDown(0)){
+//				NewRandom();
+//		         }
+//	}
+
+	//void NewRandom(){
+		//   pressed = 1;
+		 void OnMouseOver()
     {
-    	if (Input.GetMouseButtonDown(0)) {
-			pressed = 0; }
-
-
-			 if (Input.GetMouseButtonDown(0) && pressed == 0){
-				NewRandom();
-		         }
-	}
-
-	void NewRandom(){
-		   pressed = 1;
+    	if (Input.GetMouseButtonDown(0)){
            System.Random random = new System.Random();
            string quote = options[Random.Range(0, options.Length)];
            display.text = quote;
-	}
+	} }
 
 }
 
---
+++ Assets/textpanel3.cs
@@ -0,0 +1,85 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+
+public class textpanel3 : MonoBehaviour {
+public Text display;
+public Camera Camera1; 
+string[] options = 
+{"  \n...I must\navenge my\nbrother...",
+"...If I sneak\n  around\nthe back...",
+"\n\n  ...Why?...",
+" \n\n         ...",
+" \n\n  ...This is\n my duty...",
+"\n\n  ...This is        \n    war...",
+"\n...Player\none started\nit...",};
+
+//
+//public int pressed = 0;
+
+	// Use this for initialization
+	void Start () {
+		   System.Random random = new System.Random();
+           string quote = options[Random.Range(0, options.Length)];
+           display.text = quote;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		    Ray ray = Camera1.ScreenPointToRay(Input.mousePosition);
+			if (Input.GetMouseButtonDown(0) && (Input.mousePosition.x > (Screen.width*.6)) && (Input.mousePosition.y > (Screen.height*.6))){
+				NewRandom();
+			 Debug.Log(Input.mousePosition.ToString("F4"));
+			}
+
+    /* if (Input.GetKey(KeyCode.Return)) 
+			{
+				if(GameObject.Find("byebye") == null) {
+			    display.text = "Player 2 wins"; }
+			} 
+
+		if(GameObject.Find("byebye") != null){ */
+
+			
+		
+		//}
+	}		
+	
+
+	// void PointerEnter()
+  //  {
+    //	if (Input.GetMouseButtonDown(0)) {
+	//		pressed = 0; }
+
+
+			 //if (Input.GetMouseButtonDown(0) && pressed == 0){
+				//NewRandom();
+		         //}
+
+//		     if (Input.GetMouseButtonDown(0)){
+	//			NewRandom();
+//		         }
+	//}
+
+	void NewRandom(){
+		//   pressed = 1;
+	//	 void OnMouseOver()
+   // {
+    	//if (Input.GetMouseButtonDown(0)){
+           System.Random random = new System.Random();
+           string quote = options[Random.Range(0, options.Length)];
+           display.text = quote;
+	} 
+//}
+
+}
+
+
+
+				
+
+
+		     
+	
